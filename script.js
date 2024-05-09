25246d152cd16efc41f16af6e206550171dd5d87
Daniel-santos941 <danieldepaulo.941@gmail.com>
2024-05-09 20:43:19 -0300

    melhorando a tabuada

---
+++ script.js
@@ -0,0 +1,31 @@
+let inputData = document.getElementById("number")
+let generatorButton = document.getElementById("calc")
+let finalresult = document.getElementById("result")
+
+
+generatorButton.addEventListener("click", function() {
+    if (inputData.value === ""){
+        alert("Insira um número")
+    }
+
+    if (inputData.value.length > 3) {
+        alert("Só é permitido até três algarismos!")
+        return
+    }
+
+    const numero = Number(inputData.value)
+    if (isNaN(numero)) {
+        alert("Não é permitido outra coisa a não ser números!")
+    }
+
+    let numerosParaCalculo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    for (let calculo of numerosParaCalculo) {
+        let result = numero * calculo
+        let mensagem = `${numero} x ${calculo} = ${result}\n`
+        console.log(mensagem)
+        
+        finalresult.value += mensagem
+    }
+
+})
