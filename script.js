e642a0e15359673dcff14dd305abfc5798a9d6f2
Daniel-santos941 <danieldepaulo.941@gmail.com>
2024-05-12 13:50:07 -0300

    adicionando notificações

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 let inputData = document.getElementById("number")
 let generatorButton = document.getElementById("calc")
+let toast = document.getElementById("toast")
+let statusInfo = document.getElementById("status")
+let info = document.getElementById("info")
+let statusBar = document.getElementById("status-bar")
+let iconStatus = document.getElementById("icon-status")
 let finalresult = document.getElementById("result")
-
 
 generatorButton.addEventListener("click", function() {
     if (inputData.value === ""){
-        alert("Insira um número")
+        statusBar.style.backgroundColor = "#F79202"
+        iconStatus.innerHTML = '<i style="color: #F79202;" class="fa-solid fa-circle-exclamation"></i>'
+        statusInfo.innerHTML = "Atenção!"
+        info.innerHTML = "Insira um número"
+        toast.classList.add("visible")
+        setTimeout(function() {
+            toast.classList.remove("visible")
+        }, 5000)
         return
     }
 
     if (inputData.value.length > 3) {
-        alert("Só é permitido até três algarismos!")
+        statusBar.style.backgroundColor = "#DE1B23"
+        iconStatus.innerHTML = '<i style="color: #DE1B23;" class="fa-solid fa-circle-xmark"></i>'
+        statusInfo.innerHTML = "Erro!"
+        info.innerHTML = "Só é permitido até três algarismos!"
+        toast.classList.add("visible")
+        setTimeout(function() {
+            toast.classList.remove("visible")
+        }, 5000) 
         return
     }
 
     const numero = Number(inputData.value)
     if (isNaN(numero)) {
-        alert("Não é permitido outra coisa a não ser números!")
+        statusBar.style.backgroundColor = "#DE1B23"
+        iconStatus.innerHTML = '<i style="color: #DE1B23;" class="fa-solid fa-circle-xmark"></i>'
+        statusInfo.innerHTML = "Erro!"
+        info.innerHTML = "Não é permitido outra coisa a não ser números!"
+        toast.classList.add("visible")
+        setTimeout(function() {
+            toast.classList.remove("visible")
+        }, 5000) 
+        return
     }
 
     let numerosParaCalculo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+  
     for (let calculo of numerosParaCalculo) {
         let result = numero * calculo
         let mensagem = `${numero} x ${calculo} = ${result}\n`
