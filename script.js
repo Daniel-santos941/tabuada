7f3d02c7587c80b23db9e39e4a8a95ecc94dfeca
Daniel-santos941 <danieldepaulo.941@gmail.com>
2024-05-12 14:39:10 -0300

    reparando as alterações

--- script.js
+++ script.js
@@ -9,40 +9,46 @@ let finalresult = document.getElementById("result")
 
 generatorButton.addEventListener("click", function() {
     if (inputData.value === ""){
-        statusBar.style.backgroundColor = "#F79202"
-        iconStatus.innerHTML = '<i style="color: #F79202;" class="fa-solid fa-circle-exclamation"></i>'
-        statusInfo.innerHTML = "Atenção!"
-        info.innerHTML = "Insira um número"
-        toast.classList.add("visible")
-        setTimeout(function() {
-            toast.classList.remove("visible")
-        }, 5000)
-        return
-    }
-
-    if (inputData.value.length > 3) {
-        statusBar.style.backgroundColor = "#DE1B23"
-        iconStatus.innerHTML = '<i style="color: #DE1B23;" class="fa-solid fa-circle-xmark"></i>'
-        statusInfo.innerHTML = "Erro!"
-        info.innerHTML = "Só é permitido até três algarismos!"
-        toast.classList.add("visible")
-        setTimeout(function() {
-            toast.classList.remove("visible")
-        }, 5000) 
+        toastExecution(
+            "#F79202",
+            "exclamation",
+            "Atenção",
+            "Insira um número!"
+        )
         return
     }
 
     const numero = Number(inputData.value)
     if (isNaN(numero)) {
-        statusBar.style.backgroundColor = "#DE1B23"
-        iconStatus.innerHTML = '<i style="color: #DE1B23;" class="fa-solid fa-circle-xmark"></i>'
-        statusInfo.innerHTML = "Erro!"
-        info.innerHTML = "Não é permitido outra coisa a não ser números!"
-        toast.classList.add("visible")
-        setTimeout(function() {
-            toast.classList.remove("visible")
-        }, 5000) 
+        toastExecution(
+            "#DE1B23",
+            "xmark",
+            "Erro!",
+            "Não é permitido outra coisa a não ser números!"
+        )
         return
+    }
+    
+    if (numero.toString().length > 3) {
+        toastExecution(
+            "#DE1B23",
+            "xmark",
+            "Erro!",
+            "Só é permitido até três algarismos!"
+        )
+        return
+    } else if (numero === 0) {
+        toastExecution(
+            "#F79202", 
+            "exclamation", 
+            "Atenção", 
+            "Todo o número multiplicado por zero é zero!"
+        ) 
+        return
+    }   
+
+    if (finalresult.value) {
+        finalresult.value = ""
     }
 
     let numerosParaCalculo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -50,9 +56,19 @@ generatorButton.addEventListener("click", function() {
     for (let calculo of numerosParaCalculo) {
         let result = numero * calculo
         let mensagem = `${numero} x ${calculo} = ${result}\n`
-        console.log(mensagem)
-        
+          
         finalresult.value += mensagem
     }
-
+    
 })
+
+function toastExecution(color, iconName, status, description) {
+    statusBar.style.backgroundColor = color
+    iconStatus.innerHTML = `<i style="color: ${color};" class="fa-solid fa-circle-${iconName}"></i>`
+    statusInfo.innerHTML = status
+    info.innerHTML = description
+    toast.classList.add("visible")
+    setTimeout(function() {
+        toast.classList.remove("visible")
+    }, 5000)
+}
